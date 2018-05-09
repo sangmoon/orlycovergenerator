823bdad8174093a1708fee3c2583da2479c7a639
sangmoon <cholsoo22001@gmail.com>
2018-05-09 13:02:22 +0900

    사용자 입력에 받아서 그림 그려주기 추가

--- cover/forms.py
+++ cover/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from urllib.parse import urlencode
 
 
 class CoverForm(forms.Form):
     title = forms.CharField()
     top_text = forms.CharField()
     author = forms.CharField()
+
+    @property
+    def get_params(self):
+        if hasattr(self, "cleaned_data"):
+            return urlencode(self.cleaned_data)
+        else:
+            return None
--- cover/views.py
+++ cover/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CoverForm
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 
 # Create your views here.
@@ -10,7 +11,6 @@ def index(request):
         form = CoverForm(request.POST)
         if form.is_valid():
             form.cleaned_data  # 이미지를 생성할 데이터는 여기 있다.
-            return redirect("cover:index")
     else:
         form = CoverForm()
     return render(request, "cover/index.html", {
@@ -19,9 +19,22 @@ def index(request):
 
 
 def image_generator(request):
-    im = Image.new('RGB', (256,256), 'yellow')
+    title = request.GET['title']
+    top_text = request.GET['top_text']
+    author = request.GET['author']
 
+    im = Image.new('RGB', (256, 256), 'white')
+
+    ttf_path = settings.ROOT('assets', 'fonts', 'NanumGothicCoding.ttf')
     # 여기서 데이터를 받아서 그리겠습니다.
-    response = HttpResponse(content_type='image/jpeg')
-    im.save(response, format='JPEG')
+    fnt = ImageFont.truetype(ttf_path, 40)
+    d = ImageDraw.Draw(im)
+
+    d.text((10, 10), title, font=fnt, fill=(0, 255, 0, 128))
+    d.text((10, 60), top_text, font=fnt, fill=(0, 255, 0, 255))
+    d.text((10, 100), author, font=fnt, fill=(0, 255, 0, 255))
+
+
+    response = HttpResponse(content_type='image/png')  # file-like
+    im.save(response, format='png')
     return response

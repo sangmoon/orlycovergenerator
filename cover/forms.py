a0c8e02034d1ea1187ab4222ca4775a40ab7907a
sangmoon <cholsoo22001@gmail.com>
2018-05-10 22:57:53 +0900

    색과 그림 크롤링

--- cover/forms.py
+++ cover/forms.py
@@ -6,6 +6,17 @@ class CoverForm(forms.Form):
     title = forms.CharField()
     top_text = forms.CharField()
     author = forms.CharField()
+    animal_code = forms.ChoiceField(choices=[(i, i) for i in range(1, 41)])
+    color_code = forms.ChoiceField(choices=[(i, i) for i in range(0, 17)])
+    guide_text = forms.CharField()
+    guide_text_placement = forms.ChoiceField(
+        choices=[
+            ('botoom_right', 'botoom_right'),
+            ('bottom_left', 'bottom_left'),
+            ('top_right', 'top_right'),
+            ('top_left', 'top_left'),
+        ]
+    )
 
     @property
     def get_params(self):

8e130e1167ad291ec4e6fa7133368ef9f3a8bfb9
sangmoon <cholsoo22001@gmail.com>
2018-05-18 16:28:14 +0900

    오타 수정...

--- covergenerator/settings/prod.py
+++ covergenerator/settings/prod.py
@@ -1,8 +1,8 @@
 from .common import *
 
 
-STATICFILES_STORAGES = 'covergenerator.storages.StaticStorage'
-DEFAULT_FILE_STORAGES = 'covergenerator.storages.MediaStorage'
+STATICFILES_STORAGE = 'covergenerator.storages.StaticStorage'
+DEFAULT_FILE_STORAGE = 'covergenerator.storages.MediaStorage'
 
 AZURE_ACCOUNT_NAME = os.environ['AZURE_ACCOUNT_NAME']
 AZURE_ACCOUNT_KEY = os.environ['AZURE_ACCOUNT_KEY']
